2a1bfa4492cb9720c639b52ea350b647fe430064
erdem-hash <erdem@halalbooking.com>
2025-10-17 10:47:35 +0300

    Add files via upload

---
+++ label.tsx
@@ -0,0 +1,6 @@
+
+import * as React from "react";
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+export function Label({ className, ...props }: LabelProps) {
+  return <label className={"text-sm font-medium text-slate-700 " + (className || "")} {...props} />;
+}
